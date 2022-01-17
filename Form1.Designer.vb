0b5b79ee7667708bbebe6526b80dd9721202cd38
Luciano Lisboa <lucianoicraft@gmail.com>
2022-01-16 23:53:09 -0300

    Carga.

---
+++ Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmTestaOracle
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSTART = New System.Windows.Forms.Button()
+        Me.txtRESULT = New System.Windows.Forms.TextBox()
+        Me.pnlBTN = New System.Windows.Forms.Panel()
+        Me.pnlRESULT = New System.Windows.Forms.Panel()
+        Me.pnlBTN.SuspendLayout()
+        Me.pnlRESULT.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnSTART
+        '
+        Me.btnSTART.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnSTART.Location = New System.Drawing.Point(0, 0)
+        Me.btnSTART.Name = "btnSTART"
+        Me.btnSTART.Size = New System.Drawing.Size(96, 261)
+        Me.btnSTART.TabIndex = 0
+        Me.btnSTART.Text = "INICIA TESTE"
+        Me.btnSTART.UseVisualStyleBackColor = True
+        '
+        'txtRESULT
+        '
+        Me.txtRESULT.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtRESULT.Location = New System.Drawing.Point(0, 0)
+        Me.txtRESULT.Multiline = True
+        Me.txtRESULT.Name = "txtRESULT"
+        Me.txtRESULT.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtRESULT.Size = New System.Drawing.Size(488, 261)
+        Me.txtRESULT.TabIndex = 1
+        Me.txtRESULT.WordWrap = False
+        '
+        'pnlBTN
+        '
+        Me.pnlBTN.Controls.Add(Me.btnSTART)
+        Me.pnlBTN.Dock = System.Windows.Forms.DockStyle.Right
+        Me.pnlBTN.Location = New System.Drawing.Point(488, 0)
+        Me.pnlBTN.Name = "pnlBTN"
+        Me.pnlBTN.Size = New System.Drawing.Size(96, 261)
+        Me.pnlBTN.TabIndex = 2
+        '
+        'pnlRESULT
+        '
+        Me.pnlRESULT.Controls.Add(Me.txtRESULT)
+        Me.pnlRESULT.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlRESULT.Location = New System.Drawing.Point(0, 0)
+        Me.pnlRESULT.Name = "pnlRESULT"
+        Me.pnlRESULT.Size = New System.Drawing.Size(488, 261)
+        Me.pnlRESULT.TabIndex = 3
+        '
+        'frmTestaOracle
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 261)
+        Me.Controls.Add(Me.pnlRESULT)
+        Me.Controls.Add(Me.pnlBTN)
+        Me.Name = "frmTestaOracle"
+        Me.Text = "TestaOracle"
+        Me.pnlBTN.ResumeLayout(False)
+        Me.pnlRESULT.ResumeLayout(False)
+        Me.pnlRESULT.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSTART As Button
+    Friend WithEvents txtRESULT As TextBox
+    Friend WithEvents pnlBTN As Panel
+    Friend WithEvents pnlRESULT As Panel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports Icraft.IcftBase
+Imports System.Windows.Forms
+Public Class frmTestaOracle
+    Private Sub BtnSTART_Click(sender As Object, e As EventArgs) Handles btnSTART.Click
+        Dim nomeArq As String = ""
+        Dim rodada As Integer = 0
+        Dim Retorno As String = ""
+        Dim SFD As New SaveFileDialog()
+        SFD.DefaultExt = "txt"
+        SFD.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        SFD.FileName = "log"
+        SFD.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+        If SFD.ShowDialog() = DialogResult.OK Then
+            'pega o nome do arquivo
+            nomeArq = SFD.FileName
+        Else
+            Exit Sub
+        End If
+        System.IO.File.WriteAllText(nomeArq, "")
+        Dim escreveArq As System.IO.StreamWriter = System.IO.File.AppendText(nomeArq)
+        Try
+            While True
+                Application.DoEvents()
+                rodada += 1
+                'AtualizaStatus("Tentativa de recuperar valor do Oracle")
+                Try
+                    Retorno = DSValor("count(*)", "sbdb.sócio", "strConn", "")
+                Catch ex As Exception
+                    'AtualizaStatus("Falha ao tentar recuperar valor do Oracle")
+                    Retorno = "ERRO:" & ex.Message.ToString.ToUpper
+                End Try
+
+                Retorno = rodada & " | " & Retorno & " - " & DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")
+                escreveArq.WriteLine(Replace(Retorno, System.Environment.NewLine, " "))
+                txtRESULT.AppendText(System.Environment.NewLine & Retorno)
+                System.Threading.Thread.Sleep(5000) ' 5 segundos
+            End While
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString.ToUpper)
+        End Try
+        escreveArq.Close()
+    End Sub
+End Class
